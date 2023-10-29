e1c06b78bb7a229e3e5f40d14e229d8d8ef2c44f
ilgamkhisam <ilgamkhisam@gmail.com>
2023-10-29 16:25:36 +0600

    todoproject

---
+++ todolist/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class ToDo(models.Model):
+    title = models.CharField('Название задания', max_length=170)
+    is_complete = models.BooleanField('Завершено', default=False)
+
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural ='Задания'
+
+    def __str__(self) -> str:
+        return self.title 
+    
